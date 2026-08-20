95162a57ba2643b75fc2b396d6e0dd714e295b54
tadikalayaswanthkumar-sys <tadikalayaswanthkumar@gmail.com>
2026-08-20 22:13:08 +0530

    javascript basic operation are performed

---
+++ python/javascript/basics/operation.js
@@ -0,0 +1,9 @@
+// let a = 10;
+// let b = 20;
+// console.log(a + b);
+// console.log(a-b);
+let score1 = 85;
+let score2 = 90;
+let score3 = 78;
+let avg = (score1 + score2 + score3) / 3;
+console.log(avg)
